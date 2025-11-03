42448c2d9e509d7caf69740a9322dc5a17939836
dudekula777 <dudekula777@gmail.com>
2025-11-03 13:12:08 +0530

    vm

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,8 +1,3 @@
 terraform {
-  backend "azurerm" {
-    resource_group_name  = "tfstate-rg"
-    storage_account_name = "tfstatestorageacc"
-    container_name       = "tfstate"
-    key                  = "dev/terraform.tfstate"
-  }
+  backend "azurerm" {}
 }
--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -1,8 +1,3 @@
 terraform {
-  backend "azurerm" {
-    resource_group_name  = "tfstate-rg"
-    storage_account_name = "tfstatestorageacc"
-    container_name       = "tfstate"
-    key                  = "prod/terraform.tfstate"
-  }
+  backend "azurerm" {}
 }
