bd6b5a2fb5010db4ce9ba487ffa31d7385c45655
dudekula777 <dudekula777@gmail.com>
2025-11-03 16:16:34 +0530

    updated

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,7 +1,10 @@
 # Provider configuration
 provider "azurerm" {
   features {}
-
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
 }
 
 provider "tls" {}
